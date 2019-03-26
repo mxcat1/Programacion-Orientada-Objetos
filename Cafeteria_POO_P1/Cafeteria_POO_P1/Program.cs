bcb5e77cd0e563925b40e67b29e9107e553b3443
mxcat1 <mxcat.xxtremxx@gmail>
2019-03-26 18:31:11 -0500

    Primer Proyecto Abstraccion de una Cafeteria POO

---
+++ Cafeteria_POO_P1/Cafeteria_POO_P1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafeteria_POO_P1
+{
+    class Cafeteria{
+    //Propiedades
+        //Unas Listas especificando el los muebles el equipo y el ambiente
+        List<Mueble> Mobiliaria { get; set; }
+        List<Equipo> Herramientas { get; set; }
+        List<Ambiente> Ambientes { get; set; }
+    }
+    class Ambiente{
+
+    }
+    class Equipo{
+
+    }
+    class Caja{
+
+    }
+    class Mueble{
+
+    }
+    class Producto{
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

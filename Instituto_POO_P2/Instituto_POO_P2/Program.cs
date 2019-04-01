4214a65618d39b39e90b982008830f70bdbfe3dd
CHRISTIAN NICOLL VILCA APAZA <c.vilca.apaza@isur.edu.pe>
2019-04-01 13:10:14 -0500

    Incristado nuevo codigo para aulas incompleto

--- Instituto_POO_P2/Instituto_POO_P2/Program.cs
+++ Instituto_POO_P2/Instituto_POO_P2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Christian Vilca Apaza
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -78,10 +79,64 @@ namespace Cafeteria_Isur
 
 namespace Instituto_POO_P2
 {
+    class Edificio
+    {
+        List<Aula> Aulas { get; set; }
+    }
     class Aula
     {
-
+        float[] Color = new float[4];
+        List<Mueble> Mobiliaria { get; set; }
+        List<Material> Materiales { get; set; }
+        List<Luz> Luces { get; set; }
     }
+    class Mueble
+    {
+        String id_Mueble { get; set; }
+        TipoMueble Tipo { get; set; }
+        NroPatas Patas { get; set; }
+    }
+    enum TipoMueble
+    {
+        Ninguno=0,Silla=1,Mesa=2,Tacho=3
+    }
+    enum NroPatas
+    {
+        Ninguno=0,Patas4=2
+    }
+    class Material
+    {
+        TipoMaterial Tipo { get; set; }
+    }
+    enum TipoMaterial
+    {
+        Ninguno=0,Computador=1,Plumon=2,Mota=3,Proyector=4, Pizarra = 5
+    }
+    class Luz
+    {
+        String Marca { get; set; }
+        TipoLuz TipoLuces { get; set; }
+    }
+    enum TipoLuz
+    {
+        Ninguno=0,Fluorecente=1,Bombilla=2,Led=3
+    }
+    //class Silla:Mueble
+    //{
+
+    //}
+    //class Mesa : Mueble
+    //{
+
+    //}
+    //class Pizarra:Mueble
+    //{
+
+    //}
+    //class Tacho : Mueble
+    //{
+
+    //}
     class Program
     {
         static void Main(string[] args)

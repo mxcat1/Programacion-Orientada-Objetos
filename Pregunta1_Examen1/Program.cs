0fb2f31446fd82db59eff679c3f0bcfdceb00297
Visual Studio <vstudio@isur.edu.pe>
2019-04-03 12:54:37 -0500

    nuevo

---
+++ Pregunta1_Examen1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pregunta1_Examen1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

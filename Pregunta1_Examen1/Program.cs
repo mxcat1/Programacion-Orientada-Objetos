1671831d76306a61a846ed09f08f3f90421a0c4e
mxcat1 <mxcat.xxtremxx@gmail>
2019-04-09 10:19:53 -0500

    se cambio la clase libro pòr producto donde se lleva revistas periodicos etc se agrgo la clase enum

--- Pregunta1_Examen1/Program.cs
+++ Pregunta1_Examen1/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace Biblioteca_Isur
 {
+
     class Persona{
         public string NomPersona{get;set;}
         public string ApePersona{get;set;}
@@ -16,8 +17,9 @@ namespace Biblioteca_Isur
 
     }
     class Autor:Persona{
-        //Un Autor Puede tener varios Libros
-        public List<Libro> ListaLibros {get;set;}
+        //Un Autor Puede tener varios Libros,Revistas ETC
+        // Se cambio la lista de plibros por la de productos
+        public List<Producto> ListaProductos {get;set;}
         public Autor(){
             ListaLibros = new List<Libro>();
         }
@@ -38,18 +40,47 @@ namespace Biblioteca_Isur
     enum Cargo{
         Ninguno=0,Gerente=1,Encargado=2
     }
+    class Producto{
+        public string NombrePro {get;set;}
+        public TipoProducto ProductoTipo {get;set;}
+        public List<Autor> Autores {get;set;}
+        public Editorial EditorialLibro {get;set;}
+        public Producto(){
+            Autores = new List<Autor>();
+        }
 
+    }
+    // se cambio la clse libro por producto donde se lleva 
+    // revistas periodicos etc
+    // se agrego la clse enum para poder aumentar
+    enum TipoProducto{
+        Ninguno=0,Libro=1,Revistas=2,Periodico=3,etc=5
+    }
 
-    class Libro{
+/*     class Libro:Producto{
         //un Libro puede tener uno o mas Autores
         public List<Autor> Autores {get;set;}
         public Editorial EditorialLibro {get;set;}
         public Libro(){
             Autores = new List<Autor>();
         }
-    }
+    } */
     class Editorial{
 
+    }
+    class Operaciones{
+        public void Prestamos{
+
+        }
+        public void Sanciones{
+
+        }
+        public void EntradaProducto{
+
+        }
+        public void Restauraciones{
+
+        }
     }
     class Program
     {

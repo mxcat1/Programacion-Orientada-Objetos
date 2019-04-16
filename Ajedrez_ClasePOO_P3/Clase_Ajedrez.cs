22a95f7051aa40155b0d62481aa73cbe37b871f8
Your Name <you@example.com>
2019-04-16 12:57:03 -0500

    nuevo

---
+++ Ajedrez_ClasePOO_P3/Clase_Ajedrez.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Ajedrez
+{
+    public class Coordenada{
+        public int x;public int y;
+    }
+    public class Pieza{
+
+        public Color ColorPieza{get;set;}
+        public bool Activo{get;set;}
+        public bool PuedeAscender{get;set;}
+        public Coordenada Posicion {get;set;}
+        //------------- Metodos
+        public void Mover(){
+
+        }
+        public void Matar(){
+
+        }
+    }
+    public class Peon:Pieza{
+
+    }
+    public class Caballo:Pieza{
+        
+    }
+    public class Alfil:Pieza{
+        
+    }
+    public class Rey:Pieza{
+        
+    }
+    public class Reina:Pieza{
+        
+    }
+    public class Torre:Pieza{
+        
+    }
+    class Persona{
+
+    }
+    class Jugador:Persona{
+
+    }
+    class Casillero{
+
+    }
+    class Tablero{
+        Casillero[,] TableroCasillero;
+        public Tablero(){
+
+        }
+    }
+    class JuegoAjedrez{
+        Tablero TableroJuegoAjedrez {get;set;}
+        Jugador[] _Jugadores;
+
+        public JuegoAjedrez(){
+            _Jugadores = new Jugador();
+        }
+    }
+
+}
---
+++ Ajedrez_ClasePOO_P3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajedrez_ClasePOO_P3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

3332760dcb85e59ed332f79914c1ba17b0c95013
mxcat1 <mxcat.xxtremxx@gmail>
2019-04-21 21:00:31 -0500

    Agregado nuevos propiedades y la fucion definir

--- Constructora_POO4_C/Edificios.cs
+++ Constructora_POO4_C/Edificios.cs
@@ -10,10 +10,24 @@ namespace Constructora_POO4_C
     // public abstract class UnidadHabitacional{
 
     // }
+    public class Dimencionm2{
+        //Solo Terreno Regular
+        public float Ancho{get;set;}
+        public float Largo{get;set;}
+        public float Area{get;set;}
+        public void CalcularArea(){
+            Area=Ancho*Largo;
+        }
+    }
+    public enum TipoHabitacion{
+        ninguna=0,Baño=1,Cuarto=2,Comedor=3
+    }
     public class Habitacion:Piso{
+        public TipoHabitacion TipoHabitacion {get;set;}
     }
     public class Piso{
-
+        public Dimencionm2 DimecionPisoHabitacion{get;set;}
+        public float AlturaPisoHabitacion{get;set;}
     }
     public class Edificio{
         // UnidadHabitacional[][] _UnidadHabitaciolaEdificio;
@@ -31,9 +45,12 @@ namespace Constructora_POO4_C
             // for (int i=0;i<nHabitaciones;i++){
             //     _Pisos[nPiso][i]=new Habitacion[nHabitaciones];
             // }
+
         }
-        public void AgregarHabitaciones(){
-            
+        public void AgregarHabitaciones(int nPiso,int nHabitaciones){
+            for (int i=0;i<nHabitaciones;i++){
+                PisosEdificios[nPiso][i]=new Habitacion();
+            }
         }        
     }
 

3efd8219e2ccffea2277c57629e379bfe50ff2d8
Visual Studio <vstudio@isur.edu.pe>
2019-05-29 13:00:39 -0500

    guardar

--- PolimorfismoP1_POO28519/PolimorfismoP1_POO28519/FIgura.cs
+++ PolimorfismoP1_POO28519/PolimorfismoP1_POO28519/FIgura.cs
@@ -13,6 +13,10 @@ namespace PolimorfismoP1_POO28519
         {
             return area;
         }
+        public virtual void DibujarFigura()
+        {
+            Console.WriteLine("hola");
+        }
     }
     class Rectangulo : Figura
     {
@@ -31,12 +35,24 @@ namespace PolimorfismoP1_POO28519
             get { return ancho; }
             set { ancho = value; }
         }
+        public Rectangulo(double l,double a)
+        {
+            Largo = l;
+            Ancho = a;
+        }
         public override double CalcularArea()
         {
             area = Largo * Ancho;
             return area;
         }
-
+        public override void DibujarFigura()
+        {
+            Console.WriteLine("------------");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+            Console.WriteLine("------------");
+        }
     }
     class Circunferencia : Figura
     {
@@ -47,10 +63,22 @@ namespace PolimorfismoP1_POO28519
             get { return radio; }
             set { radio = value; }
         }
+        public Circunferencia(double radio)
+        {
+            Radio = radio;
+        }
         public override double CalcularArea()
         {
 
             return area=Math.PI*(radio*radio);
+        }
+        public override void DibujarFigura()
+        {
+            Console.WriteLine("--/--------");
+            Console.WriteLine("-/----------");
+            Console.WriteLine("|-----------");
+            Console.WriteLine("'\'----------");
+            Console.WriteLine("-'\'---------");
         }
 
 
@@ -72,9 +100,25 @@ namespace PolimorfismoP1_POO28519
             get { return ancho; }
             set { ancho = value; }
         }
+        public Triangulo(double al, double a)
+        {
+            Altura = al;
+            Ancho = a;
+        }
+        //public static implicit operator Triangulo(double[] datos)
+        //{
+        //    return new Triangulo(datos[1],datos[2]);
+        //}
         public override double CalcularArea()
         {
             return (area=Altura*Ancho)/2;
+        }
+        public override void DibujarFigura()
+        {
+            Console.WriteLine("        *        ");
+            Console.WriteLine("       ***       ");
+            Console.WriteLine("      *****      ");
+            Console.WriteLine("     *******     ");
         }
     }
 }
--- PolimorfismoP1_POO28519/PolimorfismoP1_POO28519/Program.cs
+++ PolimorfismoP1_POO28519/PolimorfismoP1_POO28519/Program.cs
@@ -10,6 +10,28 @@ namespace PolimorfismoP1_POO28519
     {
         static void Main(string[] args)
         {
+            var hola = new{ a=1, b=2, d=3,e="hola" };
+            Figura[] arraysfigura = new Figura[5];
+            //Triangulo tri1 = new double[2] {5,10 };
+            Figura rect1 = new Rectangulo(15,20);
+            Figura rect2 = new Rectangulo(20,25);
+            Figura tria1 = new Triangulo(3,5);
+            Figura tria2 = new Triangulo(2,6);
+            Figura tria3 = new Triangulo(5,5);
+
+            arraysfigura[0]= rect1;
+            arraysfigura[1]= rect2;
+            arraysfigura[2]= tria1;
+            arraysfigura[3]= tria2;
+            arraysfigura[4]= tria3;
+
+            for (int i=0;i<5;i++)
+            {
+                Console.WriteLine(arraysfigura[i].CalcularArea());
+                arraysfigura[i].DibujarFigura();
+                Console.WriteLine("katy estuvo aqui xd");
+            }
+            Console.ReadLine();
         }
     }
 }

cb6ee4a4dc5c709adf7e2ef69c9a775d43afb356
mxcat1 <mxcat.xxtremxx@gmail>
2019-05-28 20:07:38 -0500

    Polimorfizmo sobreescritura

---
+++ AjedrezPOO_AgregadoPolimorfismo/AjedrezPOO_AgregadoPolimorfismo/Ajedrez.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Ajedrez
+{
+    public class Coordenada
+    {
+        public string x; public int y;
+    }
+    public class Pieza
+    {
+
+        public Color ColorPieza { get; set; }
+        public bool Activo { get; set; }
+        // public bool PuedeAscender{get;set;}
+        public Coordenada Posicion { get; set; }
+        //------------- Metodos
+        // con el metodo virtual para poder sobreescribir
+        public virtual void Mover()
+        {
+
+        }
+        public virtual void Matar()
+        {
+
+        }
+    }
+    public enum TipoPieza
+    {
+        ninguna = 0, Caballo = 1
+    }
+
+    public class Peon : Pieza
+    {
+        public bool PuedeAscender { get; set; }
+        //Metodo mover q se sobreescribio
+        public override void Mover()
+        {
+            base.Mover();
+        }
+    }
+    public class Caballo : Pieza
+    {
+        public override void Mover()
+        {
+            base.Mover();
+        }
+    }
+    public class Alfil : Pieza
+    {
+        public override void Mover()
+        {
+            base.Mover();
+        }
+    }
+    public class Rey : Pieza
+    {
+        public override void Mover()
+        {
+            base.Mover();
+        }
+    }
+    public class Reina : Pieza
+    {
+        public override void Mover()
+        {
+            base.Mover();
+        }
+    }
+    public class Torre : Pieza
+    {
+        public override void Mover()
+        {
+            base.Mover();
+        }
+    }
+    // class Persona{
+
+    // }
+    class Jugador
+    {
+        public int IdJugador { get; set; }
+        public string NickName { get; set; }
+        public Peon[] PeonJugador;
+        public Reina ReinaJugador;
+        public Rey ReyJugador;
+
+
+    }
+    class Casillero
+    {
+        public string Vertical { get; set; }
+        public int Horizontal { get; set; }
+    }
+    class Tablero
+    {
+        Casillero[,] TableroCasillero;
+        public Tablero()
+        {
+            TableroCasillero = new Casillero[8, 8];
+        }
+    }
+    class JuegoAjedrez
+    {
+        Tablero TableroJuegoAjedrez { get; set; }
+        Jugador[] _Jugadores;
+
+        public JuegoAjedrez(int CantidadJugadores)
+        {
+            _Jugadores = new Jugador[CantidadJugadores];
+        }
+    }
+
+}

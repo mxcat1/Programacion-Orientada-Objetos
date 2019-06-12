a96096b9c4e4aa489ba41fbca1f2356434ac2ffc
Visual Studio <vstudio@isur.edu.pe>
2019-06-12 12:53:41 -0500

    Practica clase

---
+++ Practica1POOV1Part2/Practica1POOV1Part2/Agencia.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta1
+{
+    interface ICliente
+    {
+        int Clave { get; set; }
+        string Nombre { get; set; }
+        string Rfc { get; set; }
+        string DireccionFiscal { get; set; }
+        string Telefono { get; set; }
+        string CorreoElectronico { get; set; }
+
+
+    }
+    class Honorarios<T>
+    {
+        public T Cliente;
+        public Honorarios(T Cli)
+        {
+            Cliente = Cli;
+        }
+        public static implicit operator Honorarios<T>(T Cli)
+        {
+            return new Honorarios<T>(Cli);
+        }
+    }
+    abstract class Cliente:ICliente
+    {
+        public int Clave { get; set; }
+        public string Nombre { get; set; }
+        public string Rfc { get; set; }
+        public string DireccionFiscal { get; set; }
+        public string Telefono { get; set; }
+        public string CorreoElectronico { get; set; }
+        private double Pago;
+
+        public void RegistrarPago(double p)
+        {
+            this.Pago = p;
+        }
+    }
+    class CliContado:Cliente
+    {
+        private tipopago TipoPagoCli;
+        public void RegistrarTipoPago(tipopago t)
+        {
+            this.TipoPagoCli = t;
+        }
+    }
+    class CliCredito : Cliente
+    {
+        public double LimiteCredito { get; set; }
+        public DateTime PlazoMaximo { get; set; }
+    }
+    enum tipopago
+    {
+        ninguno=0,efectivo=1,cheque=2,transferencia=3
+    }
+}
---
+++ Practica1POOV1Part2/Practica1POOV1Part2/Banco.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta2
+{
+    class Cliente<T>
+    {
+        public string ClaveCli { get; set; }
+        public string NombreCli { get; set; }
+        private T Cuenta;
+
+        public Cliente(string clave,string nombre,T cuenta)
+        {
+            this.ClaveCli = clave;
+            this.NombreCli = nombre;
+            this.Cuenta = cuenta;
+        }
+
+    }
+    interface ICuenta
+    {
+        double NCuenta { get; set; }
+        double SaldoCuenta { get; set; }
+        string ConsultarDatos();
+        double Depositar();
+        string Retirar();
+    }
+    abstract class Cuenta:ICuenta
+    {
+        protected double _NCuenta;
+        protected double _SaldoCuenta;
+
+        public double NCuenta { get => _NCuenta; set => _NCuenta=value; }
+        public double SaldoCuenta { get => _SaldoCuenta; set => _SaldoCuenta=value; }
+
+        public string ConsultarDatos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Depositar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual string Retirar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class CuentaCheques:Cuenta
+    {
+        public double ComisionUsoChequera { get; set; }
+        public double ComisionEmisionChequera { get; set; }
+
+
+    }
+    class CuentaAhorros:Cuenta
+    {
+        public DateTime FecVec { get; set; }
+        public double InteresMensual { get; set; }
+        private void DepoInt()
+        {
+
+        }
+        public override string Retirar()
+        {
+
+            if (DateTime.Now == FecVec)
+            {
+                return "Se Retiro con Exito";
+            }
+
+            return "No se Puede Retirar Por la Fecha";
+        }
+    }
+}

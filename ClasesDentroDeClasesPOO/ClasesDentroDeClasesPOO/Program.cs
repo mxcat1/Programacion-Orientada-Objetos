d434124038c4bdd122776b12f3c931f2a9013c64
Visual Studio <vstudio@isur.edu.pe>
2019-06-19 13:12:02 -0500

    DymanicObject

---
+++ ClasesDentroDeClasesPOO/ClasesDentroDeClasesPOO/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesDentroDeClasesPOO
+{
+    class huevo
+    {
+
+    }
+    class Matrioshka<T>
+    {
+        T _boneca;
+        string Nombre { get;set; }
+     
+        public Matrioshka(string name="Matrioshka", T matrioshka=default(T))
+        {
+            _boneca = matrioshka;
+            Nombre = name;
+        }
+        public string Contenido(T m= default(T))
+        {
+            var contenido = "";
+            if (this._boneca != null || m!=null)
+            {
+                contenido += Contenido(_boneca);
+            }
+            else
+            {
+                contenido += this.Nombre;
+            }
+            return contenido;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrioshka<huevo> M1 = new Matrioshka<huevo>();
+            Console.WriteLine(M1.Contenido());
+            Console.ReadLine();
+        }
+    }
+}

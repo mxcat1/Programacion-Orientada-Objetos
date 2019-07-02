29a0a2ee128a7702eadd640ca5502e0396efff99
Visual Studio <vstudio@isur.edu.pe>
2019-07-02 07:51:39 -0500

    Programa tarea

---
+++ UniversoPOO02072019/UniversoPOO02072019/Universo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversoPOO02072019
+{
+    class EnteAstronomico
+    {
+        public string nombre { get; set; }
+        public double Diametro { get; set; }
+    }
+    class Universo
+    {
+        public Galaxias[] galaxias;
+    }
+    class Planeta:EnteAstronomico
+    {
+        
+        public Estrella OrbitaEstrella;
+    }
+    class Galaxias : EnteAstronomico
+    {
+        public Planeta[] planetas;
+    }
+    class Estrella : EnteAstronomico
+    {
+
+    }
+    class Satelites
+    {
+
+    }
+    class Asteroides
+    {
+
+    }
+    class Cometas
+    {
+
+    }
+}

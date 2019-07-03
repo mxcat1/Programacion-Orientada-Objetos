3249846dad9e0b5e3ad62eeee4c3b44594a83ce4
Visual Studio <vstudio@isur.edu.pe>
2019-07-03 13:58:19 -0500

    Añadir eventos para practicar

---
+++ DelegadosPOO3719/DelegadosPOO3719/Clases.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosPOO3719
+{
+    class Clases
+    {
+        //Metodo como Delegado
+        public static void MetodoRadio(string Mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Estamos en la clase radio");
+            Console.WriteLine($"Este es un mensaje {Mensaje}");
+        }
+
+        public delegate void Metodo(string Mensaje);
+    }
+    class Pastel
+    {
+        //Metodo como Delegado
+        public static void MostrarPastel(string anuncio)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Estamos en la clase Pastel");
+            Console.WriteLine($"Este es un mensaje Pastel {anuncio}");
+        }
+    }
+
+    public delegate void dDescongelado(int grados);
+    public delegate void dKilosReservasBajas(int kilos);
+
+    class CRefri
+    {
+
+
+
+        private dDescongelado _delDescongelado;
+        private dKilosReservasBajas _delReservasBajas;
+
+        private int _Grados;
+        private int _KilosAlimento;
+        public CRefri(int k,int g)
+        {
+            _Grados = g;
+            _KilosAlimento = k;
+        }
+        //Multicasting
+        public void AdicionarMetodoReserva(dKilosReservasBajas pmetodo)
+        {
+            _delReservasBajas += pmetodo;
+        }
+        //public void AdicionarMetodoReserva(dKilosReservasBajas pmetodo)
+        //{
+        //    _delReservasBajas = pmetodo;
+        //}
+        public void EliminarMetodoReserva(dKilosReservasBajas pmetodo)
+        {
+            _delReservasBajas -= pmetodo;
+        }
+        public void AdicionarMetodoDescongelado(dDescongelado pmetodo)
+        {
+            _delDescongelado += pmetodo;
+        }
+        //public void AdicionarMetodoDescongelado(dDescongelado pmetodo)
+        //{
+        //    _delDescongelado = pmetodo;
+        //}
+        public void EliminarMetodoDescongelado(dDescongelado pmetodo)
+        {
+            _delDescongelado -= pmetodo;
+        }
+        
+        public int KilosAlimento { get { return _KilosAlimento; } }
+        public int Grados { get { return _Grados; } }
+
+
+        public void Trabajar(int pConsumo)
+        {
+            _KilosAlimento -= pConsumo;
+            _Grados += 1;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"{KilosAlimento} Kilos {Grados} Grados");
+            if (KilosAlimento < 10)
+            {
+                _delReservasBajas(KilosAlimento);
+            }
+            if (Grados > 0)
+            {
+                _delDescongelado(Grados);
+            }
+        }
+
+    }
+    class CTienda
+    {
+        public static void MandarViveres(int pkilos)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Estamos en la clase Tienda");
+            Console.WriteLine($"Viveres por {pkilos} Kilos");
+        }
+    }
+}
---
+++ DelegadosPOO3719/DelegadosPOO3719/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosPOO3719
+{
+    public delegate void MiDelegado(string mensaje);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MiDelegado delegado= Clases.MetodoRadio;
+            //delegado("Hola a todos");
+            //delegado += Pastel.MostrarPastel;
+            //delegado("Feliz Cumpleaños");
+            CRefri MiRefri = new CRefri(70, -20);
+            Random rnd = new Random();
+
+            //MiRefri.AdicionarMetodoDescongelado(InformeGrados); para el anterior ejemplo
+            //MiRefri.AdicionarMetodoReserva(new CRefri.dKilosReservasBajas(InformeKilos)); para el anterior ejemplo
+
+            //CRefri.dDescongelado x = grados => InformeGrados(grados); // para otro momento no entender
+
+            dKilosReservasBajas kilos1 = new dKilosReservasBajas(InformeKilos);
+            dKilosReservasBajas kilos2 = new dKilosReservasBajas(CTienda.MandarViveres);
+
+            dDescongelado desc1 = new dDescongelado(InformeGrados);
+
+            MiRefri.AdicionarMetodoReserva(kilos1);
+            MiRefri.AdicionarMetodoReserva(kilos2);
+            MiRefri.AdicionarMetodoDescongelado(desc1);
+
+            while (MiRefri.KilosAlimento>0)
+            {
+                MiRefri.Trabajar(rnd.Next(1, 5));
+            }
+
+
+            Console.ReadLine();
+        }
+        public static void InformeKilos(int pKilos)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Reservas bajas de alimentos estoy a nivel de main");
+            Console.WriteLine($"Quedan {pKilos}");
+        }
+        public static void InformeGrados(int pGrados)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Reservas bajas de alimentos estoy a nivel de main");
+            Console.WriteLine($"Estan a {pGrados} Grados");
+        }
+    }
+
+}
